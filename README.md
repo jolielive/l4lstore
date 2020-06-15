6c6f4fa2b5b316d5a39a5caa3248bf3b23643697
Ironic <nick@ironicsuccess.com>
2020-06-14 23:43:35 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Live 4 Love Custom Designs
+
+> Site Created for L4L site and store
+
+[jolie@live4love.app](email Live4Love)
---
+++ gatsby-config.js
@@ -0,0 +1,51 @@
+var dotenv = require("dotenv");
+dotenv.config();
+
+const { spaceId, accessToken, snipcart } = process.env;
+
+module.exports = {
+  siteMetadata: {
+    title: `Live4Love`,
+    description: `Live 4 Love Custom Designs`,
+    author: `@jolielive`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Live4Love`,
+        short_name: `live4love`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/loveicon.png`,
+      },
+    },
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId,
+        accessToken
+      }
+    },
+    {
+      resolve: "gatsby-plugin-snipcartv3",
+      options: {
+        apiKey: snipcart,
+        autopop: true,
+      },
+    },
+  ],
+}
